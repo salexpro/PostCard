b4e2f61ce58675a21bc16b98864021a6e266e2a9
salexpro <dev@salex.pro>
2017-02-19 05:09:07 +0200

    main page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 // $(document).foundation();
+
+// Menu
+$('.head_hamb').click(function(){
+    $('.head_hamb, .head_nav').toggleClass('is_active');
+});
+
+// Custom select
+$('.select7').select7()
+
+// Play/pause video
+var video = $('.main_video')[0];
+$('.main_video_button').click(function(){
+    video.paused ? video.play() : video.pause();
+})
